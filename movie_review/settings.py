e44cb5c7f97d69eeff75dca16a4267f4f0840591
cmkishores <cmkishores@gmail.com>
2019-09-20 23:21:52 +0530

    Add two apps reviews and users

--- movie_review/settings.py
+++ movie_review/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+    #local_apps
+
+    'users.apps.UserConfig',
+    'reviews.apps.ReviewsConfig',
 ]
 
 MIDDLEWARE = [
