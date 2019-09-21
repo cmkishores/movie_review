3a7fc407d9dbcc710efb3ea873e559bd15fd81b2
cmkishores <cmkishores@gmail.com>
2019-09-21 09:40:05 +0530

    Add CustomUserAdmin and changed authorization model in project settings

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.admin import UserAdmin
+
+
+class CustomUserAdmin(UserAdmin):
+	add_form = CustomUserCreationForm
+	form = CustomUserChangeForm
+	model = AbstractUser
+
+
+admin.site.register(AbstractUser, CustomUserAdmin)
 
 # Register your models here.
