f1dfadd0b9c7b9474c5ff4f769296529d578bc74
cmkishores <cmkishores@gmail.com>
2019-09-23 16:06:36 +0530

    Made user authentication and ownership for editing and deleting posts mandatory

--- reviews/models.py
+++ reviews/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 class Reviews(models.Model):
 	RATING = [
@@ -18,6 +19,7 @@ class Reviews(models.Model):
 	author = models.CharField(max_length=20)
 	created_at = models.DateTimeField(auto_now_add=True, editable=False, null=False, blank=False)
 	last_modified = models.DateTimeField(auto_now=True, editable=False, null=False, blank=False)
+	owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
 	def __str__(self):
 		return self.movie_name
--- reviews/views.py
+++ reviews/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import ListView,DetailView
 from django.views.generic.edit import CreateView,DeleteView,UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 from django.urls import reverse_lazy
 
 from .models import Reviews
 
-class ReviewsListView(ListView):
+class ReviewsListView(LoginRequiredMixin, ListView):
 	model = Reviews
 	template_name = 'home.html'
 	context_object_name = 'reviewlist'
+	login_url='login'
 
 class ReviewsDetailView(DetailView):
 	model = Reviews
@@ -19,13 +21,25 @@ class AddReviewView(CreateView):
 	model = Reviews
 	template_name = 'addreview.html'
 	fields = ['movie_name','release_year','review','author','rating']
+	
+	def form_valid(self, form):
+		form.instance.owner = self.request.user
+		return super().form_valid(form)
 
-class EditReviewView(UpdateView):
+class EditReviewView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 	model = Reviews
 	template_name = 'updatereview.html'
 	fields = ['movie_name', 'release_year', 'review','rating']
+	
+	def test_func(self):
+		obj = self.get_object()
+		return obj.owner == self.request.user
 
-class DeleteReviewView(DeleteView):
+class DeleteReviewView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 	model = Reviews
 	template_name = 'deletereview.html'
 	success_url = reverse_lazy('home')
+
+	def test_func(self):
+		obj = self.get_object()
+		return obj.owner == self.request.user
